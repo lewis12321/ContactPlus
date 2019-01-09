eb87a3a26ea69bd7696013a229b589ab7f3fce61
TRT-LewisJ <lewis.jefferies@raytheon.co.uk>
2019-01-09 22:18:17 +0000

    Code sesh, port forwarding not working on localhost

--- payment_flow.py
+++ payment_flow.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import requests
 
 cert = ('public.pem', 'private.key')
-client_id = "c36ffb7b-e96b-4472-b3e1-5e94ab0435d7"
+client_id = "494123ab-06d6-40e4-8f65-98f6d4d47871"
 
 
 def client_assertion():
@@ -31,7 +31,7 @@ def client_credentials(client_assertion):
     }
     response = requests.request("POST", url, headers=headers, data=payload, cert=cert)
 
-    print(response.json())
+    print(response.text)
     return response.json()
 
 
@@ -98,7 +98,7 @@ def generate_request_params(payment_request):
             }
         },
         "response_type": "code id_token",
-        "redirect_uri": "https://www.google.com",
+        "redirect_uri": "http://81.2.152.3:5000",
         "state": "10d260bf-a7d9-444a-92d9-7b7a5f088208",
         "exp": datetime.now().timestamp() + 60,
         "nonce": "10d260bf-a7d9-444a-92d9-7b7a5f088208",
@@ -117,11 +117,58 @@ def generate_request_params(payment_request):
 
 
 def generate_hybrid_flow(request_param):
-
-    url = f"https://matls.as.aspsp.ob.forgerock.financial/oauth2/realms/root/realms/openbanking/authorize?response_type=code%20id_token&client_id=c36ffb7b-e96b-4472-b3e1-5e94ab0435d7&state=10d260bf-a7d9-444a-92d9-7b7a5f088208&nonce=10d260bf-a7d9-444a-92d9-7b7a5f088208&scope=openid%20payments%20accounts&redirect_uri=https://www.google.com&request={request_param}"
+    url = f"https://matls.as.aspsp.ob.forgerock.financial/oauth2/realms/root/realms/openbanking/authorize?response_type=code%20id_token&client_id=c36ffb7b-e96b-4472-b3e1-5e94ab0435d7&state=10d260bf-a7d9-444a-92d9-7b7a5f088208&nonce=10d260bf-a7d9-444a-92d9-7b7a5f088208&scope=openid%20payments%20accounts&redirect_uri=http://81.2.152.3:5000&request={request_param}"
 
     print(url)
 
+
+def get_access_token_for_payment_submission(exchange_code, client_assertion):
+    url = "https://matls.as.aspsp.ob.forgerock.financial/oauth2/realms/root/realms/openbanking/access_token"
+
+    payload = f"grant_type=authorization_code&code={exchange_code}&redirect_uri=http%3A%2F%2F81.2.152.3:5000&client_assertion_type=urn%3Aietf%3Aparams%3Aoauth%3Aclient-assertion-type%3Ajwt-bearer&client_assertion={client_assertion}"
+    headers = {
+        'Content-Type': "application/x-www-form-urlencoded",
+    }
+
+    response = requests.request("POST", url, data=payload, headers=headers, cert=cert)
+
+    print(response.text)
+    return response.json()
+
+
+def payment_submission(payment_token, payment_request):
+
+    url = "https://rs.aspsp.ob.forgerock.financial:443/open-banking/v2.0/payment-submissions"
+
+    payload = {
+        "Data": {
+            "PaymentId": f"{payment_request['Data']['PaymentId']}",
+            "Initiation": {
+                "InstructionIdentification": "ACME412",
+                "EndToEndIdentification": "FRESCO.21302.GFX.20",
+                "InstructedAmount": {
+                    "Amount": "250.88",
+                    "Currency": "GBP"
+                },
+                "CreditorAccount": {
+                    "SchemeName": "SortCodeAccountNumber",
+                    "Identification": "20793443634639",
+                    "Name": "demo"
+                }
+            }
+        },
+        "Risk": {}
+    }
+
+    headers = {
+        'Authorization': f"Bearer {payment_token['access_token']}",
+        'x-idempotency-key': "FRESCO.21302.GFX.20",
+        'x-fapi-financial-id': "0015800001041REAAY"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers, cert=cert)
+
+    print(response.text)
 
 
 client_assertion = client_assertion()
@@ -129,3 +176,21 @@ access_token = client_credentials(client_assertion)
 payment_request = create_payment_request(access_token)
 request_param = generate_request_params(payment_request)
 generate_hybrid_flow(request_param)
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/", methods=["POST"])
+def exchange():
+    print(request.data)
+    return ""
+
+
+app.run(port=55555)
+
+# payment_token = get_access_token_for_payment_submission(exchange_code, client_assertion)
+# payment_submission(payment_token, payment_request)
