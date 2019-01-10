54440bc822b660f6708ec121dd3b03b8d42a055d
TRT-LewisJ <lewis.jefferies@raytheon.co.uk>
2019-01-10 19:42:40 +0000

    creates the files required

---
+++ build.py
@@ -0,0 +1,13 @@
+import os
+
+private_key = os.environ.get('PRIVATE_KEY', 'Test Private')
+public_cert = os.environ.get('PUBLIC_CERT', 'Test Public')
+
+if os.environ.get('PROD') is 'TEST':
+    f = open("private.key", "w+")
+    f.write(private_key)
+    f.close()
+
+    f = open("public.pem", "w+")
+    f.write(public_cert)
+    f.close()
