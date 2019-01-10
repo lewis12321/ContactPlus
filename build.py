fc7321285115aa2d7857093e5de32c21e77e1112
TRT-LewisJ <lewis.jefferies@raytheon.co.uk>
2019-01-10 19:47:45 +0000

    fixed build

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ import os
 private_key = os.environ.get('PRIVATE_KEY', 'Test Private')
 public_cert = os.environ.get('PUBLIC_CERT', 'Test Public')
 
-if os.environ.get('PROD') is 'TEST':
+if os.environ.get('PROD') == 'TEST':
     f = open("private.key", "w+")
     f.write(private_key)
     f.close()
