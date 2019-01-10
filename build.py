9d5c9c847eb8d7c190f834aaeb27a056584b416e
TRT-LewisJ <lewis.jefferies@raytheon.co.uk>
2019-01-10 19:58:48 +0000

    maybe fixed again...

--- build.py
+++ build.py
@@ -1,18 +1,17 @@
 import os
 
+env = os.environ.get('PROD')
 private_key = os.environ.get('PRIVATE_KEY', 'Test Private')
 public_cert = os.environ.get('PUBLIC_CERT', 'Test Public')
-env = os.environ.get('PROD')
 
 print(env)
 print(private_key)
 print(public_cert)
 
-if env == "TEST":
-    f = open("private.key", "w+")
-    f.write(private_key)
-    f.close()
+f = open("private.key", "w+")
+f.write(private_key)
+f.close()
 
-    f = open("public.pem", "w+")
-    f.write(public_cert)
-    f.close()
+f = open("public.pem", "w+")
+f.write(public_cert)
+f.close()
