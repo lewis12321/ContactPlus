d0c20834cfbe097dc8e333b47792af564eec0cf4
TRT-LewisJ <lewis.jefferies@raytheon.co.uk>
2019-01-10 19:51:22 +0000

    testing build...

--- build.py
+++ build.py
@@ -2,8 +2,13 @@ import os
 
 private_key = os.environ.get('PRIVATE_KEY', 'Test Private')
 public_cert = os.environ.get('PUBLIC_CERT', 'Test Public')
+env = os.environ.get('PROD')
 
-if os.environ.get('PROD') == 'TEST':
+print(env)
+print(private_key)
+print(public_cert)
+
+if env == 'TEST':
     f = open("private.key", "w+")
     f.write(private_key)
     f.close()
