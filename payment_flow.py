c822b48cc1fb2a8b39a01f70ca2036818d26f860
TRT-LewisJ <lewis.jefferies@raytheon.co.uk>
2019-01-11 16:05:19 +0000

    got name wrong on temp file

--- payment_flow.py
+++ payment_flow.py
@@ -7,8 +7,8 @@ import requests
 cert = ()
 
 if os.environ.get('ENV') == "TEST":
-    public_cert = tempfile.temporaryFile(delete=False)
-    private_key = tempfile.temporaryFile(delete=False)
+    public_cert = tempfile.TemporaryFile(delete=False)
+    private_key = tempfile.TemporaryFile(delete=False)
     public_cert.write(os.environ.get('PUBLIC_CERT'))
     private_key.write(os.environ.get('PRIVATE_KEY'))
     public_cert.close()
