bd146b29aa59836cf1e22a0bc04256f94b9da884
TRT-LewisJ <lewis.jefferies@raytheon.co.uk>
2019-01-20 15:47:20 +0000

    update

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import os
 from urllib import parse
 from flask import Flask, render_template, request
+from werkzeug.utils import redirect
 from payment_flow import get_access_token_for_payment_submission, get_client_assertion, setup_payment
 
 app = Flask(__name__)
@@ -28,7 +29,7 @@ def exchange():
 def payment():
     print("Creating Payment...")
     url = setup_payment()
-    return render_template("payment.html", url=url)
+    return redirect(url)
 
 
 app.run(host="0.0.0.0", port=os.getenv('PORT', 55555))
